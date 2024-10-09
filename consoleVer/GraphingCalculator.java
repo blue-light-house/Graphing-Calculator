ac86c5a562de60c653f0b1d6f7a93b85bbc257fb
blue-light-house <camaccag@mtu.edu>
2024-10-09 13:12:24 -0400

    Begin Work
    Began implementing string parsing method

--- consoleVer/GraphingCalculator.java
+++ consoleVer/GraphingCalculator.java
@@ -1,7 +1,25 @@
-import System;
+import java.lang.System;
+import java.util.*;
 
 public class GraphingCalculator {
+	
+	public GraphingCalculator() {
+	}
+	
+	private String[] recognizer(String expression) {
+		String[] splitExpression = expression.trim().split("\\s+");
+		return splitExpression;
+	}
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
+		GraphingCalculator self = new GraphingCalculator();
+		if (args.length > 0) {
+			String[] test = self.recognizer(args[0]);
+			for (String s : test) {
+				System.out.print(s);
+			}
+			System.out.println();
+		}
     }
 }
