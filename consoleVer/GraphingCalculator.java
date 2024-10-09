75f34336e532b7bb9e456d447a282dd91ea4c970
blue-light-house <camaccag@mtu.edu>
2024-10-09 13:27:11 -0400

    First evaluate() function work
    Implemented addition function and tested

--- consoleVer/GraphingCalculator.java
+++ consoleVer/GraphingCalculator.java
@@ -11,6 +11,33 @@ public class GraphingCalculator {
 		return splitExpression;
 	}
 	
+	private double evaluate (String[] expression) throws IllegalArgumentException {
+		if(expression.length % 2 != 1) throw new IllegalArgumentException("Expression input is invalid!");
+		double toReturn = Double.parseDouble(expression[0]);
+		boolean evaluating = true;
+		int index = 1;
+		while (index < expression.length - 1) {
+			if (expression[index].equals("+")) {
+				toReturn += Double.parseDouble(expression[index+1]);
+				index +=2;
+			} else if (expression[index].equals("-")) {
+				toReturn -= Double.parseDouble(expression[index+1]);
+				index +=2;
+			} else if (expression[index].equals("*")) {
+				index +=2;
+			} else if (expression[index].equals("/")) {
+				index +=2;
+			} else if (expression[index].equals("^")) {
+				index +=2;
+				System.out.println("Coming soon!");
+			} else {
+				throw new IllegalArgumentException("Invalid operation in expression!");
+			}
+		}
+		
+		return toReturn;
+	}
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
 		GraphingCalculator self = new GraphingCalculator();
@@ -20,6 +47,7 @@ public class GraphingCalculator {
 				System.out.print(s);
 			}
 			System.out.println();
+			System.out.println(self.evaluate(test));
 		}
     }
 }
