83892a5d109b1d5b78054a7b6365a3c79609d5cf
blue-light-house <camaccag@mtu.edu>
2024-10-11 11:30:29 -0400

    Update GraphingCalculatorTest.java
    Add basic test gases to GraphingCalculatorTest for string splitter and recognizer

--- consoleVer/GraphingCalculatorTest.java
+++ consoleVer/GraphingCalculatorTest.java
@@ -1,1 +1,23 @@
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class GraphingCalculatorTest {
+	GraphingCalculator self = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		self = new GraphingCalculator;
+	}
+	
+	@Test
+	public void testRecognizer1() {
+		assertArrayEquals(self.recognizr("3 + 5"), ["3", "+", "5"]);
+	}
+	
+	@Test
+	public void testRecognizer2]() {
+		assertArrayEquals(self.recognizr("3 + 5 * 2 / 8 + 4.5"), ["3", "+", "5", "*", "2", "/", "8", "+", "4.5"]);
+	}
+}
